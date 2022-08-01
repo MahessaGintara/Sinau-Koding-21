1bd0803e70ee47e9396222229810755e55d90eee
MahessaGintara <mahessagintara@gmail.com>
2022-08-01 16:17:45 +0700

    tugas 2

---
+++ bilanganPrima.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan bilangan : ");
+        int number = input.nextInt();
+        
+        for (int i = 0; i <= number; i++) {
+            mencariBilanganPrima(i);
+        }
+    }
+    public static void mencariBilanganPrima(int n) {
+        int i, flag = 0;
+        for (i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0) {
+            System.out.println(n+" Bilangan prima");
+        } else {
+            System.out.println(n+" Bukan bilangan prima");
+        }
+    }
+
+}
