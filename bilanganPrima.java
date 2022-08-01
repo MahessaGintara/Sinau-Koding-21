8f9c174963c3e5dde1f44ed383a71da4f2fa9021
MahessaGintara <mahessagintara@gmail.com>
2022-08-02 05:48:34 +0700

    Mengganti nama kelas dan menutup input

--- bilanganPrima.java
+++ bilanganPrima.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
-public class Main {
+public class bilanganPrima {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Masukkan bilangan : ");
         int number = input.nextInt();
         
+        input.close();
+
         for (int i = 0; i <= number; i++) {
             mencariBilanganPrima(i);
         }
     }
     public static void mencariBilanganPrima(int n) {
         int i, flag = 0;
-        for (i = 2; i <= n / 2; ++i) {
+        for (i = 2; i <= n / 2 ; ++i) {
             if (n % i == 0) {
                 flag = 1;
                 break;
